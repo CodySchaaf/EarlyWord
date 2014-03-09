614fe1d83b4e5a1f103b6f7ac0c3fd9e9b2e33ac
Cody Schaaf <cody@csc8.com>
2014-03-09 14:25:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EarlyBird::Application.config.secret_key_base = '8620a0fde71a89139ae1c8657328f86e6c0ce20d709d2df6d1f5a403a58c36e5ebd2867e46a467ead42311bac463e342a44f85bd3d2c539aeae0792f600498bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EarlyBird::Application.config.session_store :cookie_store, key: '_early_bird_session'

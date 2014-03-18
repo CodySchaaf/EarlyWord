3e980ac7e7934072eb34c09a1378f743a85bfbc0
Cody Schaaf <cody@csc8.com>
2014-03-17 18:38:48 -0700

    Added very mvp email functionality on sign Up.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+	before_filter :update_sanitized_params, if: :devise_controller?
+
 	def new
 		@zip_code = Weather.new
 		super
@@ -34,9 +36,14 @@ class RegistrationsController < Devise::RegistrationsController
 			@weather
 		end
 
+		def update_sanitized_params
+			devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+		end
+
 	private
 
 		def weather_params
 			params.require(:weather).permit(:zip_code)
 		end
+
 end

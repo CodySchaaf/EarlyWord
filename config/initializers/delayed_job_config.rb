787b6ebf7b3bafd410b131990c4ec3cccf84a718
Cody Schaaf <cody@csc8.com>
2014-04-30 22:35:26 -0700

    Added delayed jobs initializer file in an attempt to fix the memory issue on heroku, I reduced how often the jobs run

---
+++ config/initializers/delayed_job_config.rb
@@ -0,0 +1,7 @@
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 30.minutes
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 1.minutes
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
